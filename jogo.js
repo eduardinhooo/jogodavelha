2d3df570cd28fa82229f2a2fd1c35e366cd6a916
eduardinhooo <129527327+eduardinhooo@users.noreply.github.com>
2023-10-27 10:48:32 -0300

    jogo

---
+++ jogo.js
@@ -0,0 +1,9 @@
+// seleciona todos os elementos com a classe "celula" e os armazena
+const celulas = document.querySelectorAll(".celula")
+
+let checarTurno = true;
+
+let turno;
+
+const JOGADOR_X = "X";
+const JOGADOR_0 = "0";
